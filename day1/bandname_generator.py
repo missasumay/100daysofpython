721ba9c2dc4bba9757044ed7ecac4c7f43473998
missasumay <missasumay@outlook.com>
2024-02-26 19:04:31 +0100

    day 1

---
+++ day1/bandname_generator.py
@@ -0,0 +1,8 @@
+#greeting
+print("Welcome to the Band Name Generator!")
+#collect city name
+city_name = input("What's the name of a city you grew up in:\n")
+#collect pet name
+pet_name = input("What's your pet's name?\n")
+#generate band name
+print("Your Band Name is: " + city_name + " " + pet_name)
